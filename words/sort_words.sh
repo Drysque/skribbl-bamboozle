9bff2d39f25228b1e23552e990562b86c1fb91c1
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-04-01 22:01:52 +0200

    Added shell script to sort words

---
+++ words/sort_words.sh
@@ -0,0 +1,32 @@
+FILE=words/words.txt
+JSFILE=src/words.js
+
+#check source existence
+test -f $FILE
+if (( $? != 0 )) ; then
+    stat $FILE ; exit 1
+fi 
+
+# create tmp file
+TMPFILE=mktemp
+
+# sort word by length
+cat $FILE | awk '{ print length, $0 }'  | sort -n -s | cut -d" " -f2- > $TMPFILE
+
+# put tmp in js file
+mv $TMPFILE $FILE
+
+# redirect everything to tmp
+exec 1>$TMPFILE
+
+# put js code in tmp file
+echo "export function getWords() {"
+echo "    // DO NOT ADD WORDS HERE. PLEASE USE SHELL SCRIPT"
+echo "    return ["
+while read -r line ; do
+    echo "        '$line',"
+done < $FILE
+echo -e "    ];\n}"
+
+# put tmp in js file
+mv $TMPFILE $JSFILE
