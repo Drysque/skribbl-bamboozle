cf6879b91bddfca7b812ab2a3bf50482213ed6ac
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-04-01 22:29:20 +0200

    Added clear button

--- src/popup.js
+++ src/popup.js
@@ -8,3 +8,14 @@ bamboozle_button.onclick = () => {
         });
     });
 };
+
+const clear_button = document.getElementById('clear_button');
+
+// clear in popup
+clear_button.onclick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+            code: 'console.clear()',
+        });
+    });
+};
