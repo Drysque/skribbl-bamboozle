c879e4cb049a92f1953875e1784e96e33f7fd1b5
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-04-02 10:02:17 +0200

    Added more words

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ bamboozle = async () => {
     words = (await getWords()).filter(word => word.match(wordRegex) !== null);
 
     if (words.length === 1) fillWord(words[0]);
-    else printPossibilities(words);
+    printPossibilities(words);
 };
 
 if (getNLettersHidden() != 0) bamboozle();
